a4428c5f3827b36a3e6b05f52bae9942d0202c52
alejandrotarragona <101907175+alejandrotarragona@users.noreply.github.com>
2022-10-14 17:28:50 -0300

    Add files via upload

---
+++ pwa-tarragona/scripts/app.js
@@ -0,0 +1,61 @@
+let weather = {
+  apiKey: "b1d1d55736b570a59ed50597bb571f80",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No funciona prueba otro.");
+          throw new Error("No funciona prueba otro.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  //llamdamos alas clases
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Tiempo en  " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humedad: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "velocidad del viento: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("buenos aires");
+
+// async function requestHandler(req, res){
+//   const user = await user.findById(req.userid);
+//   task.findById(user.taskid);
+//   task.completed=true
+// }
